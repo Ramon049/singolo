2c7b8add998eecea2b39eaf45972c8d45e1a7726
Roman Koshla <koshla.roman10@gmail.com>
2020-03-21 01:53:19 +0400

    fix: form sumbmit reset and header

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 const MENU = document.getElementById('nav-menu');
 const SUBMIT = document.getElementById('submit');
+const FORM = document.getElementById('form');
 const CLOSE_BUTTON = document.getElementById('close_button');
 const PORTFOLIO_BUTTON = document.getElementById('portfolio-navBar');
 const IPHONE_BUTTON_V = document.getElementById('iphone_button');
@@ -12,14 +13,39 @@ const RIGHT_SLIDE = document.querySelector('.slider_block-right_arrow');
 const LEFT_SLIDE = document.querySelector('.slider_block-left_arrow');
 let direction;
 
+document.addEventListener('scroll', onScroll);
+
+function onScroll(event) {
+    const curPos = window.scrollY;
+    const sect = document.querySelectorAll('.content .anchor');
+    const links = document.querySelectorAll('#nav-menu a');
+
+    sect.forEach((el) => {
+        console.log(el.offsetTop, curPos, el.offsetHeight)
+        if(el.offsetTop - 95 <= curPos && (el.offsetTop - 95 + el.offsetHeight)  > curPos) {
+            links.forEach((a) => {
+                a.classList.remove('active');
+                if(el.getAttribute('id') === a.getAttribute('href').substring(1)) {
+                    a.classList.add('active')
+                }
+            })
+        }
+    })
+}
+
 MENU.addEventListener('click', (event) => {
     MENU.querySelectorAll('a').forEach(el => el.classList.remove('active'));
     event.target.classList.add('active');
 });
 
-SUBMIT.addEventListener('click', (e) => {
+FORM.addEventListener('submit', (e) => {
     e.preventDefault();
+    return false;
+});
+
+SUBMIT.addEventListener('click', (e) => {
     if(document.getElementById('name').value && document.getElementById('email').value) {
+        e.preventDefault();
         const subject = document.getElementById('subject').value.toString();
         const project = document.getElementById('project').value.toString();
         document.getElementById('res1').innerText = 'Письмо отправлено';
@@ -46,6 +72,7 @@ CLOSE_BUTTON.addEventListener('click', () => {
     document.getElementById('res3').innerText = '';
     document.getElementById('message_block').classList.add('none');
     document.getElementById('message').classList.add('none');
+    FORM.reset();
 });
 
 IPHONE_BUTTON_V.addEventListener('click', () => {
